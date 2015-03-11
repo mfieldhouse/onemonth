af1061235c314254b27cdec6a6b9d4fbf7ea7e27
Matt Fieldhouse <fieldhouse@gmail.com>
2015-03-11 01:21:30 +0000

    Test commit for heroku

--- db/migrate/20150310190948_devise_create_users.rb
+++ db/migrate/20150310190948_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false
       t.string :encrypted_password, null: false
+      # test comment
 
       ## Recoverable
       t.string   :reset_password_token
